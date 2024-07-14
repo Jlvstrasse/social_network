1db7b8e9edddc082d8869540268f1b8566226ed2
Jlvstrasse <jlvstrasse@gmail.com>
2024-07-14 16:24:07 -0400

    server bug

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -8,11 +9,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social_network', {
- 
+  
 });
 
 mongoose.set('debug', true);
 
 app.use(require('./routes'));
+
+// Add a route to handle the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the Social Network API');
+});
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
